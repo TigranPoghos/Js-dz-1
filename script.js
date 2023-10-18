3fd9ed708a7c70aa0e1d8a25d52944d805ecd9fc
TigranPoghos <tigran2002poghosyan@gmail.com>
2023-10-18 17:55:41 +0300

    dz
    finish

---
+++ script.js
@@ -0,0 +1,166 @@
+// let old = prompt("Введите ваш возраст", '')
+
+// if ((old>0) && (old<12)){
+//     alert ("ребенок")
+// } else if ((old>12) && (old<18)){
+//     alert ("подросток")
+// } else if ((old>18) && (old<60)){
+//     alert ("взрослый")
+// } else if (old>60){
+//     alert ("пенсионер")
+// }
+
+
+
+
+
+
+
+// let value = prompt("Введите число от 0 до 9", '')
+
+// if (value===1){
+//     alert("!")
+// } else if (value==2){
+//     alert("@")
+// } else if (value==3){
+//     alert("#")
+// } else if (value==4){
+//     alert("$")
+// } else if (value==5){
+//     alert("%")
+// } else if (value==6){
+//     alert("^")
+// } else if (value==7){
+//     alert("&")
+// } else if (value==8){
+//     alert("*")
+// } else if (value==9){
+//     alert("(")
+// } else if (value==0){
+//     alert(")")
+// }
+
+
+
+
+
+
+
+
+// let value = prompt('Введите число от 100 до 999', "")
+
+// if (value[0] == value[1] || value[0] == value[2] || value[1] == value[2])
+// {
+//     alert('есть совпадения')
+// }
+// else
+//     alert('нет совпадений')
+
+
+
+
+
+
+
+
+// let year = prompt('Введите число', "")
+
+// if (((year%4 == 0) || (year%400 == 0)) && (year%100 != 0)){
+//     alert ("высокосный") 
+// } else alert ("невысокосный")
+
+
+
+
+
+
+
+
+// let value = prompt('Введите число от 10000 до 99999', "")
+
+// if (value[0] == value[4] && value[1] == value[3]) {
+//     alert("палиндром")
+// } else alert ("не палиндром")
+
+
+
+
+
+
+
+
+// let dollar = prompt('Введите кол-во USD:', "")
+// let convert = prompt('EUR = 1, UAH = 2, AZN = 3', "")
+
+// switch(convert) {
+//     case '1':
+//         alert (dollar*0.95 + ' EUR')
+//         break
+//     case '2':
+//         alert (dollar*36.61 + ' UAH')
+//         break
+//     case '3':
+//         alert (dollar*1.70 + ' AZN')
+//         break
+// }
+
+
+
+
+
+
+
+
+// let value = prompt('Введите сумму', "")
+
+// if ((value>200) && (value<=300)){
+//     alert (value*0.97)
+// } else if ((value>300) && (value<=500)){
+//     alert (value*0.95)
+// } else if (value>700){
+//     alert (value*0.93)
+// }
+
+
+
+
+
+
+
+
+// без понятия, формулу не знаю
+
+
+
+
+
+
+
+
+
+// let country = prompt('Где находится остров Науру: 1-В Океании 2-В Азии 3-В центральной Америке', "")
+// let capital = prompt('Столица Норвегии: 1-Амстердам 2-Осло 3-Хельсинки', "")
+// let flag = prompt('Какие страны имеют одинаковые флаги: 1-Ирландия и Кот-д ивуар 2-Монако и Польша 3-Чад и Румыния', "")
+
+// let score = 0
+
+// if (country == 1){
+//     score += 2
+// }
+// if (capital == 2){
+//     score += 2
+// }
+// if (flag == 3){
+//     score += 2
+// }
+
+// alert(score)
+
+
+
+
+
+
+
+
+// Не знаю
